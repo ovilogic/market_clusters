bc05183afe5b95706131a7db520ebb5e9319108a
Ovidiu Oprea <ovidiu.opreah@yahoo.com>
2026-04-18 22:45:24 +0100

    Saved data locally, so I don't need an internet connection to work on it. I also pared it down to only 4 rows so I can better understand the calculations. I replicated the standard deviation manually, to better understand it.

--- market_behaviour.py
+++ market_behaviour.py
@@ -4,6 +4,7 @@ import matplotlib.pyplot as plt
 import pandas as pd
 from sklearn.cluster import KMeans
 from sklearn.preprocessing import StandardScaler
+import math
 
 
 # apple = yfinance.Ticker('AAPL')
@@ -14,6 +15,66 @@ TICKERS = [
     "HD", "PG", "DIS", "BAC", "XOM",
     "KO", "PEP", "INTC", "CSCO", "ORCL"
 ]
-data = yf.download(tickers=TICKERS[0: 2], start="2023-01-01", end="2024-01-01", auto_adjust=False)
-print(data.columns)
-print(data["Close"])
+# data = yf.download(tickers=TICKERS, start="2023-01-01", end=None, auto_adjust=False)
+# data.to_pickle("data.pkl")
+data = pd.read_pickle("data.pkl")
+# print(data.head())
+
+data = data["Adj Close"].dropna()
+# print(data.columns)
+# print(data.iloc[0, 2])
+
+# Let's reduce the size of the data to make it easier to understand the operations on it.
+data = data.iloc[0:4, 0:2]
+# print(data)
+
+
+# First feature: returns.
+returns = data.pct_change().dropna()
+# print(returns)
+
+# Average returns
+avg_returns = returns.mean()
+print(avg_returns)
+man_avg = []
+for i in returns.columns:
+    # manual = round(returns[i].mean(), 6)
+    # print(i, returns[i], len(returns[i]))
+    # print(sum(returns[i]))
+    manual = round(sum(returns[i])/len(returns[i]), 6)
+    man_avg.append(manual)
+# print("Man returns averaged: ", man_avg)
+
+# Volatility
+# print(type(returns))
+# print(returns.shape)
+# print(returns.iloc[:, 0], returns.iloc[:, 1])
+print("-"*10, returns, "-"*10, sep='\n')
+# print(returns.iloc[:, 0])
+
+print("*"*10, "Standard Deviation:", returns.std(), "*"*10, sep='\n')
+print(type(returns.std()))
+
+## Manual std deviation
+avg_ret_apple = avg_returns.iloc[0]
+avg_ret_amz = avg_returns.iloc[1]
+averages = [avg_ret_apple, avg_ret_amz]
+deviations = []
+my_std = []
+
+avg_count = 0
+for i in returns.columns:
+    # print(returns[i])
+    avg = averages[avg_count]
+    avg_count += 1
+    for j in returns[i]:
+        r_i = round(j, 6)
+        man_std = (r_i - avg)**2
+        deviations.append(man_std)
+    std_man = math.sqrt(sum(deviations)/(len(deviations) - 1))
+    my_std.append(round(std_man, 6))
+    deviations = []
+    print(f"Manual std deviation for {i}: ", round(std_man, 6))
+
+print("My std deviations: ", my_std, "\n", "Pandas std deviations: ", returns.std().values)
+
